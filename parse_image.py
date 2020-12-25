0ddd1dd98777e2e3ff515bbe4777cf588f2c4401
Ben Horrocks <ben.horrocks@fortemtech.com>
2020-12-24 17:04:21 -0700

    changed pillow version, made test image properly parse

--- parse_image.py
+++ parse_image.py
@@ -1,4 +1,5 @@
 from PIL import Image
 from pytesseract import image_to_string
-image = Image.open('./20201220_105057.jpg')
-Image.show(image)
+image = Image.open('./20201220_105057.jpg').rotate(-90)
+print(image_to_string(image))
+image.show()
